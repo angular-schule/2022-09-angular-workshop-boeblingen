0e155747b2276b2059094214c37f24d56bfd2f30
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-09-28 16:10:36 +0200

    +simple routing

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
