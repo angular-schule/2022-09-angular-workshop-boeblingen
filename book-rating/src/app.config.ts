b1a83a8c178d0641a7652f251287a263854d7773
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-19 12:03:37 +0200

    ngrx: feature state

--- book-rating/src/app.config.ts
+++ book-rating/src/app.config.ts
@@ -1,8 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { BookEffects } from './app/books/store/book.effects';
+import { bookFeature } from './app/books/store/book.reducer';
 
 
 
@@ -10,5 +15,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes /*, withDebugTracing() */),
     provideHttpClient(),
+
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge:25, logOnly: !isDevMode }),
+    provideState(bookFeature),
+    provideEffects(BookEffects)
 ]
 };
