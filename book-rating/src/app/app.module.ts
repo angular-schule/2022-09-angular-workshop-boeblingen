ef088ee55b6bc60b82bed243f129cee134b021e8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-19 10:51:28 +0200

    Standalone: Remove BooksModule

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksModule } from './books/books.module';
+import { BooksRoutingModule } from './books/books-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BooksModule } from './books/books.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BooksModule
+    BooksRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
