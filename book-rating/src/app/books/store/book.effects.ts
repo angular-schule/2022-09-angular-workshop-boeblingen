816025e5d1f60946dc3f8f949cb33bbdb3798933
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-19 15:53:14 +0200

    ngrx: effects + useDefineForClassFields

--- book-rating/src/app/books/store/book.effects.ts
+++ book-rating/src/app/books/store/book.effects.ts
@@ -1,26 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, concatMap } from 'rxjs/operators';
 import { Observable, EMPTY, of } from 'rxjs';
 import { BookActions } from './book.actions';
+import { BookStoreService } from '../book-store.service';
 
 
 @Injectable()
 export class BookEffects {
 
+  booksStore = inject(BookStoreService);
+
   loadBooks$ = createEffect(() => {
-    return this.actions$.pipe(
+    return inject(Actions).pipe(
 
       ofType(BookActions.loadBooks),
       concatMap(() =>
-        /** An EMPTY observable only emits completion. Replace with your own observable API request */
-        EMPTY.pipe(
+        this.booksStore.getBooks().pipe(
           map(books => BookActions.loadBooksSuccess({ books })),
           catchError(error => of(BookActions.loadBooksFailure({ error }))))
       )
     );
   });
-
-
-  constructor(private actions$: Actions) {}
 }
