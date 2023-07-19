e901373c40e9e0e33854cead0a1c6188dd83763d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-19 14:36:45 +0200

    ngrx reducer + unit test via ChatGPT ^^

--- book-rating/src/app/books/store/book.reducer.spec.ts
+++ book-rating/src/app/books/store/book.reducer.spec.ts
@@ -1,13 +1,38 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Book } from '../shared/book';
+import { BookActions } from './book.actions';
 import { reducer, initialState } from './book.reducer';
 
-describe('Book Reducer', () => {
-  describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
+fdescribe('Book Reducer', () => {
+  describe('loadBooks action', () => {
+    it('should set loading to true', () => {
+      const action = BookActions.loadBooks();
+      const state = reducer(initialState, action);
 
-      const result = reducer(initialState, action);
-
-      expect(result).toBe(initialState);
+      expect(state).toEqual({ ...initialState, loading: true });
     });
   });
+
+  describe('loadBooksSuccess action', () => {
+    it('should update the state books and set loading to false', () => {
+      const books: Book[] = [
+        { isbn: '1' } as Book,
+        { isbn: '2' } as Book
+      ];
+      const action = BookActions.loadBooksSuccess({ books });
+      const state = reducer(initialState, action);
+
+      expect(state).toEqual({ ...initialState, loading: false, books });
+    });
+  });
+
+  describe('loadBooksFailure action', () => {
+    it('should reset the state books to an empty array and set loading to false', () => {
+      const action = BookActions.loadBooksFailure(new HttpErrorResponse({ error: 'bar', status: 403 }));
+      const state = reducer(initialState, action);
+
+      expect(state).toEqual({ ...initialState, loading: false, books: [] });
+    });
+  });
+
 });
