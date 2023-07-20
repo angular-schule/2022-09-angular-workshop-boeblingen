31e4830dc8945765fae0bd4718a33b021d2c78f1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-20 13:44:22 +0200

    Sparsam mit Referenzen sein (Memoization!)

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -10,24 +10,35 @@ const maxRating = 5;
 export class BookRatingService {
 
   rateUp(book: Book): Book {
+
+    // early return, reference gespart
+    if(!this.rateUpAllowed(book)) {
+      return book;
+    }
+
     return {
       ...book,
-      rating: Math.min(book.rating + 1, maxRating)
+      rating: book.rating + 1
     };
   }
 
   rateDown(book: Book): Book {
+
+    if(!this.rateDownAllowed(book)) {
+      return book;
+    }
+
     return {
       ...book,
-      rating: Math.max(book.rating - 1, minRating)
+      rating: book.rating - 1
     };
   }
 
   rateUpAllowed(book: Book) {
-    return book.rating >= maxRating;
+    return book.rating < maxRating;
   }
 
   rateDownAllowed(book: Book) {
-    return book.rating <= minRating;
+    return book.rating > minRating;
   }
 }
