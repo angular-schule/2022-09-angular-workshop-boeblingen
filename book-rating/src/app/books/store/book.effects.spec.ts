ffc4a9b06ed2b977ac8d64b6563968aa9259dbcd
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-07-21 11:34:09 +0200

    ngrx + rxjs marbles

--- book-rating/src/app/books/store/book.effects.spec.ts
+++ book-rating/src/app/books/store/book.effects.spec.ts
@@ -1,29 +1,84 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { Observable, delay, of, throwError } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
 
+import { Book } from '../shared/book';
+import { BookActions } from './book.actions';
 import { BookEffects } from './book.effects';
+import { BookStoreService } from '../book-store.service';
+
+
+const testBook: Book = {
+  isbn: '000',
+  title: 'test',
+  description: 'test',
+  rating: 5,
+  price: 1
+};
 
 describe('BookEffects', () => {
   let actions$: Observable<any>;
   let effects: BookEffects;
+  let testScheduler: TestScheduler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule
-      ],
       providers: [
         BookEffects,
-        provideMockActions(() => actions$)
-      ]
+        provideMockActions(() => actions$),
+        // see https://ngrx.io/api/store/testing/provideMockStore
+        provideMockStore(),
+        {
+          provide: BookStoreService,
+          useValue: {
+            getBooks: () => of([testBook])
+          }
+        }
+      ],
+      // imports: [RouterTestingModule]
     });
 
     effects = TestBed.inject(BookEffects);
   });
 
-  it('should be created', () => {
-    expect(effects).toBeTruthy();
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('should load books', () => {
+    testScheduler.run(({ cold, hot, expectObservable }) => {
+
+      actions$ = hot('-a-|', {
+        a: BookActions.loadBooks()
+      });
+
+      expectObservable(effects.loadBooks$).toBe('-a-|', {
+        a: BookActions.loadBooksSuccess({ books: [testBook] })
+      });
+    });
+  });
+
+  it('should fail to load books on failing BookStoreService', () => {
+
+    const bs = TestBed.inject(BookStoreService);
+    const errorResponse = new HttpErrorResponse({});
+    spyOn(bs, 'getBooks').and.callFake(() => throwError(() => errorResponse));
+
+    testScheduler.run(({ cold, hot, expectObservable }) => {
+
+      actions$ = hot('-a-|', {
+        a: BookActions.loadBooks()
+      });
+
+      expectObservable(effects.loadBooks$).toBe('-a-|', {
+        a: BookActions.loadBooksFailure({ error: errorResponse })
+      });
+    });
   });
 });
